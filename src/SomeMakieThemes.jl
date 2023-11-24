63373e93a35d67795cf7fad3d1694de6ed6886fa
Garrek Stemo <8449000+garrekds@users.noreply.github.com>
2023-11-24 12:23:07 +0900

    remove retina themes

--- src/SomeMakieThemes.jl
+++ src/SomeMakieThemes.jl
@@ -66,66 +66,6 @@ function theme_keynote()
 end
 
 """
-    theme_retina()
-
-Theme for retina display on macOS. This compensation is necessary until
-Makie becomes display-independent.
-"""
-function theme_retina()
-
-    ticksize = 10
-    width = 2
-
-    Theme(
-        size = (1500, 1200),
-        fontsize = 35,
-        linewidth = 3,
-
-        Axis = (
-            xticksize = ticksize,
-            yticksize = ticksize,
-            xtickwidth = width,
-            ytickwidth = width,
-            spinewidth = width,
-            xgridwidth = width,
-            ygridwidth = width,
-        )
-    )
-end
-
-"""
-    theme_keynote_retina()
-
-Theme for retina display on macOS with bigger font sizes
-and appropriate size for use in Keynote. This compensation is necessary until
-Makie becomes display-independent.
-"""
-function theme_keynote_retina()
-    
-        ticksize = 18
-        width = 3
-    
-        Theme(
-            size = (1000, 900),
-            fontsize = 40,
-            linewidth = 4,
-            fonts = (; regular = "Helvetica"),
-    
-            Axis = (
-                xticksize = ticksize,
-                yticksize = ticksize,
-                xtickwidth = width,
-                ytickwidth = width,
-                spinewidth = width,
-                xgridwidth = width,
-                ygridwidth = width,
-                xgridvisible = false,
-                ygridvisible = false,
-            )
-        )
-    end
-
-"""
     theme_poster()
     
 Theme for a printed poster at A0 size.
@@ -175,35 +115,5 @@ function theme_manuscript()
         )
     )
 end
-
-
-"""
-    theme_manuscript()
-
-Theme for a manuscript figure for Mac retina displays.
-"""
-function theme_manuscript_retina()
-    # ticksize = 35
-    # width = 5
-
-    Theme(
-        size = (1000, 900),
-        fontsize = 40,
-        linewidth = 3,
-        fonts = (; regular = "Helvetica"),
-        Axis = (
-            spinewidth = 3,
-            xtickalign = 1,
-            ytickalign = 1,
-            xticksize = 18,
-            yticksize = 18,
-            xtickwidth = 3,
-            ytickwidth = 3,
-            xgridvisible = false,
-            ygridvisible = false,
-        )
-    )
-end
-
 
 end # module
